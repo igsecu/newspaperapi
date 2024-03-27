255e149645b0eeee6b81665b551ffdd319b57c43
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 15:25:14 -0300

    Created UsersAccount database model

---
+++ src/models/UsersAccount.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require("sequelize");
+const db = require("../../db");
+
+const UsersAccount = db.define(
+  "usersAccount",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    isBanned: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: true,
+  }
+);
+
+module.exports = UsersAccount;
