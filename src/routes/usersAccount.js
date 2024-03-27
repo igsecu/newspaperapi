1ca5ed6c223fc6d4af0b79041fb085883c32793a
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 16:51:56 -0300

    Added DELETE /account route

--- src/routes/usersAccount.js
+++ src/routes/usersAccount.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-const passport = require("passport");
-
 const usersAccountsController = require("../controllers/usersAccount");
+const authController = require("../controllers/auth");
+const logoutController = require("../controllers/logout");
 
+// Logout process
+router.get("/logout", logoutController.logout);
+// Get logged in account
+router.get("/account", usersAccountsController.getLoggedInAccount);
+// Login process
+router.post("/login", usersAccountsController.login);
 // Create account
 router.post("/account", usersAccountsController.createAccount);
+// Delete account
+router.delete(
+  "/account",
+  authController.ensureAuthenticatedUser,
+  usersAccountsController.deleteAccount
+);
 
 module.exports = router;
