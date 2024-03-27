925c96d99eb1f4910b27f14a7842b62fab9d832b
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 16:08:03 -0300

    Created createNotification service

---
+++ src/services/notifications.js
@@ -0,0 +1,17 @@
+const Notification = require("../models/Notification");
+
+// Create Notification
+const createNotification = async (id, text) => {
+  try {
+    await Notification.create({
+      accountId: id,
+      text,
+    });
+  } catch (error) {
+    throw new Error("Error trying to create notification");
+  }
+};
+
+module.exports = {
+  createNotification,
+};
