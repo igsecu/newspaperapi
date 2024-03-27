4be32b263afd59451a1cd8ba5d0488675634462f
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 18:33:44 -0300

    Added redis store to session

--- src/controllers/session.js
+++ src/controllers/session.js
@@ -1,10 +1,14 @@
-const session = require("express-session");
 require("dotenv").config();
+const session = require("express-session");
+const Redis = require("ioredis");
+const RedisStore = require("connect-redis").default;
+const redisClient = new Redis();
 
 const sessionMiddleware = session({
   secret: process.env.SESSION_SECRET,
   credentials: true,
   name: "sid",
+  store: new RedisStore({ client: redisClient }),
   resave: false,
   saveUninitialized: false,
   cookie: {
