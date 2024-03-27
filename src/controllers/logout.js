a8a52e87c0040003573fcb933ed9acb015e5b1cc
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 16:50:46 -0300

    Created logout controller

---
+++ src/controllers/logout.js
@@ -0,0 +1,21 @@
+// Logout process
+const logout = async (req, res, next) => {
+  if (!req.user) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `No user logged in`,
+    });
+  }
+  req.logout((err) => {
+    if (err) return next(err);
+
+    return res.status(200).json({
+      statusCode: 200,
+      msg: "You successfully logged out!",
+    });
+  });
+};
+
+module.exports = {
+  logout,
+};
