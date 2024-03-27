c65acfe2a07d2beaeba128905f34eb413cc37292
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 16:51:38 -0300

    Imported notificationServices and passport

--- src/controllers/usersAccount.js
+++ src/controllers/usersAccount.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 const {
   validatePassword,
@@ -10,6 +11,76 @@ const {
 
 const usersAccountsServices = require("../services/usersAccount");
 const notificationsServices = require("../services/notifications");
+
+// Get logged in account
+const getLoggedInAccount = async (req, res, next) => {
+  if (req.user && req.user.type === "USER") {
+    const account = await usersAccountsServices.getAccountById(req.user.id);
+    return res.status(200).json({
+      statusCode: 200,
+      data: account,
+    });
+  } else {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `No User Account logged in`,
+    });
+  }
+};
+
+// Login process
+const login = async (req, res, next) => {
+  if (req.user) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: "An user is already logged in!",
+    });
+  }
+
+  const { email, password } = req.body;
+
+  if (validateEmail(email)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validateEmail(email),
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: "Password is missing",
+    });
+  }
+
+  passport.authenticate("local", (error, user, info) => {
+    if (error) {
+      return next(error);
+    }
+    if (!user) {
+      if (info.statusCode === 400) {
+        return res.status(400).json({
+          statusCode: info.statusCode,
+          msg: info.msg,
+        });
+      }
+      return res.status(404).json({
+        statusCode: info.statusCode,
+        msg: info.msg,
+      });
+    }
+    req.logIn(user, (error) => {
+      if (error) {
+        console.log(error.message);
+        return next(error);
+      }
+      return res.status(200).json({
+        statusCode: 200,
+        msg: "You logged in successfully",
+      });
+    });
+  })(req, res, next);
+};
 
 // Create new account
 const createAccount = async (req, res, next) => {
@@ -87,6 +158,31 @@ const createAccount = async (req, res, next) => {
   }
 };
 
+// Delete account
+const deleteAccount = async (req, res, next) => {
+  try {
+    const accountDeleted = await usersAccountsServices.deleteAccount(
+      req.user.id
+    );
+
+    if (accountDeleted) {
+      req.logout((err) => {
+        if (err) return next(err);
+
+        return res.status(200).json({
+          statusCode: 200,
+          msg: "Account deleted successfully!",
+        });
+      });
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   createAccount,
+  login,
+  getLoggedInAccount,
+  deleteAccount,
 };
