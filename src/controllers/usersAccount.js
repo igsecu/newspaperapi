65f04cad8bb641af81d1cd9feb57f11e1abbc642
Ignacio Cunial <igsecu@hotmail.com>
2024-03-27 16:08:52 -0300

    Created createAccount controller

---
+++ src/controllers/usersAccount.js
@@ -0,0 +1,92 @@
+const bcrypt = require("bcryptjs");
+
+const {
+  validatePassword,
+  validateEmail,
+  validatePasswordConfirmation,
+  validateName,
+  validateLastname,
+} = require("../utils/index");
+
+const usersAccountsServices = require("../services/usersAccount");
+const notificationsServices = require("../services/notifications");
+
+// Create new account
+const createAccount = async (req, res, next) => {
+  const { email, password, password2 } = req.body;
+
+  try {
+    if (validateEmail(email)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validateEmail(email),
+      });
+    }
+
+    const emailExist = await usersAccountsServices.checkEmailExist(
+      email.toLowerCase()
+    );
+
+    if (emailExist) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `Email "${email}" exists! Try with another one!`,
+      });
+    }
+
+    if (validatePassword(password)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validatePassword(password),
+      });
+    }
+
+    if (validatePasswordConfirmation(password, password2)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validatePasswordConfirmation(password, password2),
+      });
+    }
+
+    // Hash Password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          return next("Error trying to create a new account");
+        }
+        try {
+          const accountCreated = await usersAccountsServices.createAccount(
+            hash,
+            email
+          );
+
+          if (accountCreated) {
+            const account = await usersAccountsServices.getAccountById(
+              accountCreated.id
+            );
+
+            await notificationsServices.createNotification(
+              account.id,
+              "Your account was created successfully!"
+            );
+
+            return res.status(201).json({
+              statusCode: 201,
+              data: account,
+              msg: "Account created successfully!",
+            });
+          }
+        } catch (error) {
+          console.log(error.message);
+          return next("Error trying to create a new account");
+        }
+      });
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = {
+  createAccount,
+};
