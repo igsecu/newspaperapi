3c80acc464a2e6a07ad282b1548e997b1f2b4e5e
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 16:41:47 -0300

    Added POST /writer/account route

--- src/routes/adminAccount.js
+++ src/routes/adminAccount.js
@@ -3,11 +3,18 @@ const router = express.Router();
 
 const adminAccountController = require("../controllers/adminAccount");
 const logoutController = require("../controllers/logout");
+const authController = require("../controllers/auth");
 
 // Logout process
 router.get("/logout", logoutController.logout);
 // Get logged in account
 router.get("/account", adminAccountController.getLoggedInAccount);
+// Create writer
+router.post(
+  "/writer/account",
+  authController.ensureAuthenticatedAdmin,
+  adminAccountController.createWriter
+);
 // Login process
 router.post("/login", adminAccountController.login);
 // Create admin account
