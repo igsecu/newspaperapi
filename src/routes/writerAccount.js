6d39ffa4a9e7c52fa2f55bfb563e77809289f54b
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 17:23:57 -0300

    Added PUT /account/image route

--- src/routes/writerAccount.js
+++ src/routes/writerAccount.js
@@ -5,11 +5,29 @@ const writerAccountController = require("../controllers/writerAccount");
 const logoutController = require("../controllers/logout");
 const authController = require("../controllers/auth");
 
+const fileUpload = require("express-fileupload");
+
 // Logout process
 router.get("/logout", logoutController.logout);
 // Get logged in account
 router.get("/account", writerAccountController.getLoggedInAccount);
 // Login process
 router.post("/login", writerAccountController.login);
+// Update profile image
+router.put(
+  "/account/image",
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: `${__dirname}/../../uploads`,
+  }),
+  authController.ensureAuthenticatedWriter,
+  writerAccountController.updateProfileImage
+);
+// Delete profile image
+/* router.put(
+  "/account/image",
+  authController.ensureAuthenticatedWriter,
+  writerAccountController.deleteProfileImage
+); */
 
 module.exports = router;
