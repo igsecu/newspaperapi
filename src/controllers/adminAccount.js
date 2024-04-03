c3993551f7236e3675697317a52f87cd61171428
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 10:41:21 -0300

    Created createAccount controller

---
+++ src/controllers/adminAccount.js
@@ -0,0 +1,85 @@
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+
+const {
+  validatePassword,
+  validateEmail,
+  validatePasswordConfirmation,
+  validateId,
+} = require("../utils/index");
+
+const adminAccountServices = require("../services/adminAccount");
+
+// Create admin account
+const createAccount = async (req, res, next) => {
+  const { email, password, password2 } = req.body;
+  try {
+    // Validations
+    if (validateEmail(email)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validateEmail(email),
+      });
+    }
+
+    const emailExist = await adminAccountServices.checkEmailExist(email);
+
+    if (emailExist) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `Email "${email}" exists! Try with another one!`,
+      });
+    }
+
+    if (validatePassword(password)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validatePassword(password),
+      });
+    }
+
+    if (validatePasswordConfirmation(password, password2)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validatePasswordConfirmation(password, password2),
+      });
+    }
+
+    // Hash Password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          return next("Error trying to create a new account");
+        }
+        try {
+          const accountCreated = await adminAccountServices.createAccount(
+            hash,
+            email
+          );
+
+          if (accountCreated) {
+            const account = await adminAccountServices.getAccountById(
+              accountCreated.id
+            );
+
+            return res.status(201).json({
+              statusCode: 201,
+              data: account,
+              msg: "Account created successfully!",
+            });
+          }
+        } catch (error) {
+          console.log(error.message);
+          return next("Error trying to create a new account");
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    return next("Error trying to create a new account");
+  }
+};
+
+module.exports = {
+  createAccount,
+};
