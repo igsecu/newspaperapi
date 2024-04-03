ed250bb43fd2fde90781de46dae525d40d37f5bb
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 16:42:29 -0300

    Created createWriter controller

--- src/controllers/adminAccount.js
+++ src/controllers/adminAccount.js
@@ -9,6 +9,7 @@ const {
 } = require("../utils/index");
 
 const adminAccountServices = require("../services/adminAccount");
+const writerAccountServices = require("../services/writerAccount");
 
 // Create admin account
 const createAccount = async (req, res, next) => {
@@ -150,8 +151,79 @@ const getLoggedInAccount = async (req, res, next) => {
   }
 };
 
+// Create writer account
+const createWriter = async (req, res, next) => {
+  const { email, password, password2 } = req.body;
+  try {
+    // Validations
+    if (validateEmail(email)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validateEmail(email),
+      });
+    }
+
+    const emailExist = await writerAccountServices.checkEmailExist(email);
+
+    if (emailExist) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: `Email "${email}" exists! Try with another one!`,
+      });
+    }
+
+    if (validatePassword(password)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validatePassword(password),
+      });
+    }
+
+    if (validatePasswordConfirmation(password, password2)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: validatePasswordConfirmation(password, password2),
+      });
+    }
+
+    // Hash Password
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          return next("Error trying to create a new account");
+        }
+        try {
+          const accountCreated = await writerAccountServices.createAccount(
+            hash,
+            email
+          );
+
+          if (accountCreated) {
+            const account = await writerAccountServices.getAccountById(
+              accountCreated.id
+            );
+
+            return res.status(201).json({
+              statusCode: 201,
+              data: account,
+              msg: "Account created successfully!",
+            });
+          }
+        } catch (error) {
+          console.log(error.message);
+          return next("Error trying to create a new account");
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    return next("Error trying to create a new account");
+  }
+};
+
 module.exports = {
   createAccount,
   login,
   getLoggedInAccount,
+  createWriter,
 };
