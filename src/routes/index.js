cc5e425eb1c85d8a3b11b70fb377234047719bbb
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 16:47:29 -0300

    Importer writers router and specified router root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const usersAccountRouter = require("./usersAccount");
 const adminAccountRouter = require("./adminAccount");
+const writerAccountRouter = require("./writerAccount");
 
 // Specify routers root routes
 router.use("/users", usersAccountRouter);
 
 router.use("/admin", adminAccountRouter);
+
+router.use("/writers", writerAccountRouter);
 
 module.exports = router;
