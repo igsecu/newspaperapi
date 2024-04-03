dec883c367c251f7c55ff117d62bd0ea56ee8895
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 10:53:25 -0300

    Added POST /login GET /account /logout routes

--- src/routes/adminAccount.js
+++ src/routes/adminAccount.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const adminAccountController = require("../controllers/adminAccount");
+const logoutController = require("../controllers/logout");
 
+// Logout process
+router.get("/logout", logoutController.logout);
+// Get logged in account
+router.get("/account", adminAccountController.getLoggedInAccount);
+// Login process
+router.post("/login", adminAccountController.login);
 // Create admin account
 router.post("/account", adminAccountController.createAccount);
 
