694c2a01bdf2e11a9a7d2491a15147efc28e5714
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 16:42:08 -0300

    Created ensurteAuthenticatedAdmin controller :

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -17,6 +17,26 @@ const ensureAuthenticatedUser = (req, res, next) => {
   }
 };
 
+// Access Control Admin
+const ensureAuthenticatedAdmin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user && req.user.type === "ADMIN") {
+      next();
+    } else {
+      return res.status(401).json({
+        statusCode: 401,
+        msg: `You are not authorized! Please login with an Admin account...`,
+      });
+    }
+  } else {
+    return res.status(401).json({
+      statusCode: 401,
+      msg: `You are not authorized! Please login...`,
+    });
+  }
+};
+
 module.exports = {
   ensureAuthenticatedUser,
+  ensureAuthenticatedAdmin,
 };
