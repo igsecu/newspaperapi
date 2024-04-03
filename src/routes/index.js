084dfd28a1601bc36e4d1c173d95a0819af19901
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 10:07:03 -0300

    Imported admin account router and specified root route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const usersAccountRouter = require("./usersAccount");
+const adminAccountRouter = require("./adminAccount");
 
 // Specify routers root routes
 router.use("/users", usersAccountRouter);
+
+router.use("admin", adminAccountRouter);
 
 module.exports = router;
