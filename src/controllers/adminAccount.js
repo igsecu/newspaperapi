1c348ef16a621c4e22f161085a5f10e307183cdd
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 10:52:14 -0300

    Created login and getLoggedInAccount controllers

--- src/controllers/adminAccount.js
+++ src/controllers/adminAccount.js
@@ -80,6 +80,78 @@ const createAccount = async (req, res, next) => {
   }
 };
 
+// Login process
+const login = async (req, res, next) => {
+  if (req.user) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: "An user is already logged in!",
+    });
+  }
+
+  const { email, password } = req.body;
+
+  if (validateEmail(email)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: validateEmail(email),
+    });
+  }
+
+  if (!password) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: "Password is missing",
+    });
+  }
+
+  passport.authenticate("admin", (error, user, info) => {
+    if (error) {
+      return next(error);
+    }
+    if (!user) {
+      if (info.statusCode === 400) {
+        return res.status(400).json({
+          statusCode: info.statusCode,
+          msg: info.msg,
+        });
+      }
+      return res.status(404).json({
+        statusCode: info.statusCode,
+        msg: info.msg,
+      });
+    }
+    req.logIn(user, (error) => {
+      if (error) {
+        console.log(error.message);
+        return next(error);
+      }
+      return res.status(200).json({
+        statusCode: 200,
+        msg: "You logged in successfully",
+      });
+    });
+  })(req, res, next);
+};
+
+// Get logged in account
+const getLoggedInAccount = async (req, res, next) => {
+  if (req.user && req.user.type === "ADMIN") {
+    const account = await adminAccountServices.getAccountById(req.user.id);
+    return res.status(200).json({
+      statusCode: 200,
+      data: account,
+    });
+  } else {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `No Admin Account logged in`,
+    });
+  }
+};
+
 module.exports = {
   createAccount,
+  login,
+  getLoggedInAccount,
 };
