2b27f164e4d309b4bca6023c18861eef62217242
Ignacio Cunial <igsecu@hotmail.com>
2024-04-03 16:57:35 -0300

    Created writers local strategy

--- src/passport/config.js
+++ src/passport/config.js
@@ -2,6 +2,7 @@ const LocalStrategy = require("passport-local").Strategy;
 
 const usersAccountsServices = require("../services/usersAccount");
 const adminAccountServices = require("../services/adminAccount");
+const writerAccountServices = require("../services/writerAccount");
 
 const bcrypt = require("bcryptjs");
 
@@ -113,6 +114,59 @@ module.exports = (passport) => {
         }
       )
     ),
+    passport.use(
+      "writer",
+      new LocalStrategy(
+        { usernameField: "email" },
+        async (email, password, done) => {
+          try {
+            // Match email
+            const accountFound = await writerAccountServices.checkEmailExist(
+              email
+            );
+            if (accountFound) {
+              // Match password
+
+              bcrypt.compare(
+                password,
+                accountFound.password,
+                async (err, isMatch) => {
+                  if (err) {
+                    return done(err, null);
+                  }
+                  if (isMatch) {
+                    const account = await writerAccountServices.getAccountById(
+                      accountFound.id
+                    );
+
+                    if (account.isBanned === true) {
+                      return done(null, false, {
+                        statusCode: 400,
+                        msg: "This account is banned! Please contact the admin of the page...",
+                      });
+                    }
+
+                    return done(null, account);
+                  } else {
+                    return done(null, false, {
+                      statusCode: 400,
+                      msg: `Incorrect password!`,
+                    });
+                  }
+                }
+              );
+            } else {
+              return done(null, false, {
+                statusCode: 404,
+                msg: `Email address not found!`,
+              });
+            }
+          } catch (error) {
+            return done(error, null);
+          }
+        }
+      )
+    ),
     passport.serializeUser((user, done) => {
       done(null, user.id);
     });
@@ -128,7 +182,13 @@ module.exports = (passport) => {
         if (adminFound) {
           done(null, { type: "ADMIN", ...adminFound });
         } else {
-          done(null, { msg: `Account not found!` });
+          const writerFound = await writerAccountServices.getAccountById(id);
+
+          if (writerFound) {
+            done(null, { type: "WRITER", ...writerFound });
+          } else {
+            done(null, { msg: `Account not found!` });
+          }
         }
       }
     } catch (error) {
