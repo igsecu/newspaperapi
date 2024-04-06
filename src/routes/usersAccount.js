643acf6bb81c98c13017d7143a93aef0a4b43eb1
Ignacio Cunial <igsecu@hotmail.com>
2024-04-06 17:48:44 -0300

    Added POST /create-subscription GET /payment/success /payment/cancel routes

--- src/routes/usersAccount.js
+++ src/routes/usersAccount.js
@@ -5,12 +5,22 @@ const usersAccountsController = require("../controllers/usersAccount");
 const authController = require("../controllers/auth");
 const logoutController = require("../controllers/logout");
 
+// Payment cancelled
+router.get("/payment/cancel", usersAccountsController.paymentCancel);
+// Payment success
+router.get("/payment/success", usersAccountsController.paymentSuccess);
 // Verify account
 router.get("/account/:id/verify", usersAccountsController.verifyAccount);
 // Logout process
 router.get("/logout", logoutController.logout);
 // Get logged in account
 router.get("/account", usersAccountsController.getLoggedInAccount);
+// Create subscription
+router.post(
+  "/create-subscription",
+  authController.ensureAuthenticatedUser,
+  usersAccountsController.createSubscription
+);
 // Login process
 router.post("/login", usersAccountsController.login);
 // Create account
