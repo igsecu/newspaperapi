a1c61446e72df2a29355f8688848b8dcaa841419
Ignacio Cunial <igsecu@hotmail.com>
2024-04-12 17:40:23 -0300

    Created getArticlesByWriter controller

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -137,9 +137,83 @@ const getArticlesBySection = async (req, res, next) => {
   }
 };
 
+// Get Articles by writer
+const getArticlesByWriter = async (req, res, next) => {
+  const { id } = req.params;
+  const { page } = req.query;
+
+  if (!validateId(id)) {
+    return res.status(400).json({
+      statusCode: 400,
+      msg: `ID: ${id} - Invalid format!`,
+    });
+  }
+
+  if (page) {
+    if (validatePage(page)) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "Page must be a number",
+      });
+    }
+
+    if (parseInt(page) === 0) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Page ${page} not found!`,
+      });
+    }
+  }
+
+  try {
+    const writerFound = await homeServices.getWriterById(id);
+
+    if (!writerFound) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Writer with ID: ${id} not found!`,
+      });
+    }
+
+    if (writerFound.isBanned === true) {
+      return res.status(400).json({
+        statusCode: 400,
+        msg: "This writer is banned! You can not access to the articles...",
+      });
+    }
+
+    const articles = await homeServices.getArticlesByWriter(
+      page ? page : 1,
+      id
+    );
+
+    if (!articles) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: "This writer does not have articles to show!",
+      });
+    }
+
+    if (!articles.data.length) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: `Page ${page} not found!`,
+      });
+    }
+
+    res.status(200).json({
+      statusCode: 200,
+      ...articles,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   getArticles,
   getArticlesMoreReaders,
   getLastArticles,
   getArticlesBySection,
+  getArticlesByWriter,
 };
