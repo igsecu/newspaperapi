9895271b0593d1d98549f1a7b53e621d0e9f2adb
Ignacio Cunial <igsecu@hotmail.com>
2024-04-12 17:29:36 -0300

    Created getArticlesBySection service

--- src/services/home.js
+++ src/services/home.js
@@ -1,5 +1,6 @@
 const Article = require("../models/Article");
 const WriterAccount = require("../models/WriterAccount");
+const Section = require("../models/Section");
 
 // Get last articles
 const getLastArticles = async () => {
@@ -170,8 +171,98 @@ const getArticles = async () => {
   }
 };
 
+// Get Section by id
+const getSectionById = async (id) => {
+  try {
+    const section = await Section.findByPk(id, {
+      attributes: ["id", "name"],
+    });
+
+    if (section) {
+      return {
+        id: section.id,
+        name: section.name,
+      };
+    }
+
+    return section;
+  } catch (error) {
+    throw new Error("Error trying to get a section by its id");
+  }
+};
+
+// Get articles by section
+const getArticlesBySection = async (page, id) => {
+  const results = [];
+  try {
+    const articles = await Article.findAndCountAll({
+      attributes: [
+        "id",
+        "title",
+        "subtitle",
+        "introduction",
+        "body",
+        "photo",
+        "comments",
+        "readers",
+        "forSubscribers",
+        "isShown",
+      ],
+      include: {
+        model: WriterAccount,
+        attributes: ["id", "email"],
+        where: {
+          sectionId: id,
+        },
+      },
+      order: [["createdAt", "DESC"]],
+      limit: 10,
+      offset: page * 10 - 10,
+    });
+
+    if (articles.count === 0) {
+      return false;
+    }
+
+    if (articles.rows.length > 0) {
+      articles.rows.forEach((a) => {
+        results.push({
+          id: a.id,
+          title: a.title,
+          subtitle: a.subtitle,
+          introduction: a.introduction,
+          body: a.body,
+          photo: a.photo,
+          comments: a.comments,
+          readers: a.readers,
+          forSubscribers: a.forSubscribers,
+          isShown: a.isShown,
+          writer: {
+            id: a.writerAccount.id,
+            email: a.writerAccount.email,
+          },
+        });
+      });
+
+      return {
+        totalResults: articles.count,
+        totalPages: Math.ceil(articles.count / 10),
+        page: parseInt(page),
+        data: results,
+      };
+    } else {
+      return { data: [] };
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error trying to get all articles by section");
+  }
+};
+
 module.exports = {
   getArticles,
   getArticlesMoreReaders,
   getLastArticles,
+  getSectionById,
+  getArticlesBySection,
 };
