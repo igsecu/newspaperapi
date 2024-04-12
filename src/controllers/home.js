b91b7a1103b2f4ac8f14ee1e15b4f617869850e7
Ignacio Cunial <igsecu@hotmail.com>
2024-04-12 12:37:53 -0300

    Created getArticlesMoreReaders controller

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,6 +3,27 @@ const homeServices = require("../services/home");
 const Redis = require("ioredis");
 const client = new Redis();
 
+// Get articles more readers
+const getArticlesMoreReaders = async (req, res, next) => {
+  try {
+    const articles = await homeServices.getArticlesMoreReaders();
+
+    if (!articles) {
+      return res.status(404).json({
+        statusCode: 404,
+        msg: "No articles to show!",
+      });
+    }
+
+    res.status(200).json({
+      statusCode: 200,
+      data: articles,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
 // Get shown not banned articles
 const getArticles = async (req, res, next) => {
   try {
@@ -14,8 +35,6 @@ const getArticles = async (req, res, next) => {
         msg: `No articles to show!`,
       });
     }
-
-    console.log("Controller");
 
     // Set data to redis
     client.setex("home_articles", 3600, JSON.stringify(articles));
@@ -31,4 +50,5 @@ const getArticles = async (req, res, next) => {
 
 module.exports = {
   getArticles,
+  getArticlesMoreReaders,
 };
