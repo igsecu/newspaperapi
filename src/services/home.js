01edc2654019671552403780294bcc5c5d46faa7
Ignacio Cunial <igsecu@hotmail.com>
2024-04-12 12:38:11 -0300

    Created getArticlesMoreReaders service

--- src/services/home.js
+++ src/services/home.js
@@ -1,6 +1,62 @@
 const Article = require("../models/Article");
 const WriterAccount = require("../models/WriterAccount");
 
+// Get articles more readers
+const getArticlesMoreReaders = async () => {
+  const results = [];
+  try {
+    const articles = await Article.findAll({
+      attributes: [
+        "id",
+        "title",
+        "subtitle",
+        "introduction",
+        "body",
+        "photo",
+        "comments",
+        "readers",
+        "forSubscribers",
+        "isShown",
+      ],
+      include: {
+        model: WriterAccount,
+        attributes: ["id", "email"],
+        where: {
+          isBanned: false,
+        },
+      },
+      order: [["readers", "DESC"]],
+      limit: 10,
+    });
+
+    if (articles) {
+      articles.forEach((a) => {
+        results.push({
+          id: a.id,
+          title: a.title,
+          subtitle: a.subtitle,
+          introduction: a.introduction,
+          body: a.body,
+          photo: a.photo,
+          comments: a.comments,
+          readers: a.readers,
+          forSubscribers: a.forSubscribers,
+          isShown: a.isShown,
+          writer: {
+            id: a.writerAccount.id,
+            email: a.writerAccount.email,
+          },
+        });
+      });
+    }
+
+    return results;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error trying to get articles with more readers");
+  }
+};
+
 // Get shown articles
 const getArticles = async () => {
   const results = [];
@@ -60,4 +116,5 @@ const getArticles = async () => {
 
 module.exports = {
   getArticles,
+  getArticlesMoreReaders,
 };
